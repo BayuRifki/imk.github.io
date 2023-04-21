283cd93b64f6806106954957ebfbf67db26ed889
b_Rky <62275405+BayuRifki@users.noreply.github.com>
2023-04-21 23:10:31 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","projek");
+
+if(!$conn){
+	echo "Gagal koneks ke database";
+} else {
+	
+};
+
+?>
